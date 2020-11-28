bfb52202e608a4d6a6711feca6e84b87f6756961
yevheniizalizniak <72947197+yevheniizalizniak@users.noreply.github.com>
2020-11-28 13:13:38 -0500

    SignInScreen.js; SignUpScreen.js; StratScreen.js
    Native React uploaded

---
+++ screens/StartScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignInScreen from './SignInScreen';
+import SignUpScreen from './SignUpScreen';
+
+const RootStack = createStackNavigator();
+
+const StartScreen = ({navigation}) => (
+    <RootStack.Navigator headerMode='none'>
+        <RootStack.Screen name="SignInScreen" component={SignInScreen}/>
+        <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/> 
+    </RootStack.Navigator>
+);
+export default StartScreen;
