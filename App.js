26dc4afb018e0f85819ba2e17c2d599a63f535ed
AlisherUrazbayev <71267989+AlisherUrazbayev@users.noreply.github.com>
2020-11-27 22:51:02 -0500

    market mockup

--- App.js
+++ App.js
@@ -23,56 +23,38 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {TouchableOpacity, TextInput, } from 'react-native'
+import Marketplace from './marketplaceUi/Marketplace';
+import MarketplaceViewItem from './marketplaceUi/Marketplace';
+import MarketPlaceViewItem from './marketplaceUi/MarketPlaceViewItem';
+import MarketplaceSell from './marketplaceUi/MarketplaceSell';
+import MarketplaceCategories from './marketplaceUi/MarketplaceCategories';
+//import Expo from 'expo';
+//import {StackNavigator} from 'react-navigation';
+
+
+
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+      <Marketplace/>
+      //<MarketPlaceViewItem/>
+      //<MarketplaceSell/>
+      //<MarketplaceCategories/>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    paddingTop: 23
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+  },
+
   scrollView: {
     backgroundColor: Colors.lighter,
   },
